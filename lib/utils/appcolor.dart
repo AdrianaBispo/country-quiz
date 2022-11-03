3b82def06158b970791422a23f4e9f9626f2cba2
dev <adriana.bispo283@gmail.com>
2022-11-03 09:15:42 -0300

    arquivo com as cores do app

---
+++ lib/utils/appcolor.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AppColor{
+static const white = Color(0xffFFFFFF);
+static const primary = Color(0xFF2F527B);
+static const secondary = Color(0xffF9A826);
+static const option= Color.fromARGB(7, 96, 102, 208); //70% opaque
+static const green = Color(0xFF60BF88);
+static const red = Color(0xFFEA8282);
+static const results = Color(0xFF1D355D);
+static const resultScore= Color(0xFF6FCF97);
+}
